f5d5fd1c79b709fb19197846c19b85128456b42b
Md Habibur Rahman Shanto <mhrshanto36@gmail.com>
2023-05-30 23:22:02 +0600

    Shopify Social Influence page

---
+++ index.js
@@ -0,0 +1,35 @@
+ function instagram() {
+  	document.getElementById("instagram-form").style.display="block";
+    document.getElementById("facebook-form").style.display="none";
+    document.getElementById("youtube-form").style.display="none";
+    document.getElementById("snap-form").style.display="none";
+    document.getElementById("blog-form").style.display="none";
+  }
+  function facebook() {
+  	document.getElementById("instagram-form").style.display="none";
+    document.getElementById("facebook-form").style.display="block";
+    document.getElementById("youtube-form").style.display="none";
+    document.getElementById("snap-form").style.display="none";
+    document.getElementById("blog-form").style.display="none";
+  }
+  function youtube() {
+  	document.getElementById("instagram-form").style.display="none";
+    document.getElementById("facebook-form").style.display="none";
+    document.getElementById("youtube-form").style.display="block";
+    document.getElementById("snap-form").style.display="none";
+    document.getElementById("blog-form").style.display="none";
+  }
+  function snap() {
+  	document.getElementById("instagram-form").style.display="none";
+    document.getElementById("facebook-form").style.display="none";
+    document.getElementById("youtube-form").style.display="none";
+    document.getElementById("snap-form").style.display="block";
+    document.getElementById("blog-form").style.display="none";
+  }
+  function blog() {
+  	document.getElementById("instagram-form").style.display="none";
+    document.getElementById("facebook-form").style.display="none";
+    document.getElementById("youtube-form").style.display="none";
+    document.getElementById("snap-form").style.display="none";
+    document.getElementById("blog-form").style.display="block";
+  }
